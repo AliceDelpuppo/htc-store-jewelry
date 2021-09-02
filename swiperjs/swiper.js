4c441a5130f12ee36497e96d8e2b4edb46a78329
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-09-02 16:48:59 -0300

    Pequena estilização

--- swiperjs/swiper.js
+++ swiperjs/swiper.js
@@ -10,3 +10,17 @@ var swiper = new Swiper(".mySwiper-main-banner", {
     mousewheel: true,
     keyboard: true,
   });
+
+//   var swiper = new Swiper(".mySwiper-varieties-gemstones", {
+//     cssMode: true,
+//     // navigation: {
+//     //   nextEl: ".swiper-button-next",
+//     //   prevEl: ".swiper-button-prev",
+//     // },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
+
